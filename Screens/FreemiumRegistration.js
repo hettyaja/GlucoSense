32b97a42e6c814388446fc02d4e24792f0aa0755
hy961 <169165271+hy961@users.noreply.github.com>
2024-05-08 22:53:55 +0800

    Freemium Register

--- Screens/FreemiumRegistration.js
+++ Screens/FreemiumRegistration.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ImageBackground, SafeAreaView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import { Image, SafeAreaView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 
 //import LoginPageBackground from '.../.../ '
 
