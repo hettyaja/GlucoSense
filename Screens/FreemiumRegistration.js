aae09fc2e91e2c9381a88a1545c63e8224b82193
hy961 <169165271+hy961@users.noreply.github.com>
2024-05-08 22:28:10 +0800

    Freemium Register

--- Screens/FreemiumRegistration.js
+++ Screens/FreemiumRegistration.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { ImageBackground, SafeAreaView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 
 //import LoginPageBackground from '.../.../ ';
-const image = {uri: 'C:\Users\Hansen Yonatan\OneDrive - SIM - Singapore Institute of Management\Pictures\UI Design'};
+const image = {uri: 'https://www.figma.com/file/81VSHMByukPMHkt0NWPm1z/Untitled?type=design&node-id=8%3A2&mode=design&t=chxWX4diUoWXY6Na-1.png'};
 
 const FreemiumRegister= () => {
     return (
@@ -13,27 +13,29 @@ const FreemiumRegister= () => {
         </View>
         <View style={{flexDirection:'row', borderBottomColor:'#ccc', marginTop: 100, marginBottom: 50}}>
         <Text style={{
-            fontFamily: 'Roboto',
-            fontSize: 20,
+            fontFamily: 'Poppins',
+            fontSize: 30,
             fontWeight: '500',
             color: '#FAF5E1',
             paddingTop: 20,
             marginTop: 30,
             marginBottom: 5,
-            paddingLeft: 5
+            paddingLeft: 5,
+            fontWeight: "bold"
             }}> Sign up </Text>
         </View>
 
         <Text style={{
             fontFamily: 'Roboto',
-            fontSize: 20,
+            fontSize: 18,
             fontWeight: '500',
             color: '#333',
-            paddingLeft: 5
+            paddingLeft: 12,
+            fontWeight: "bold"
             }}>
             Username
         </Text>
-        <View style={{ paddingLeft: 20, flexDirection:'row', borderBottomColor:'#ccc', borderBottomWidth:1, paddingBottom:2, marginBottom:10}}>
+        <View style={{ paddingLeft: 10, paddingRight: 10, flexDirection:'row', paddingBottom:2, marginBottom:10}}>
             <TextInput style={styles.input} placeholder= 'Enter your username'  keyboardType="default" />
         </View>
 
@@ -42,11 +44,12 @@ const FreemiumRegister= () => {
             fontSize: 18,
             fontWeight: '500',
             color: '#333',
-            paddingLeft: 5
+            paddingLeft: 12,
+            fontWeight: "bold",
             }}>
             Name
         </Text>
-        <View style={{ paddingLeft: 20, flexDirection:'row', borderBottomColor:'#ccc', borderBottomWidth:1, paddingBottom:2, marginBottom:15}}>
+        <View style={{ paddingLeft: 10, paddingRight: 10, flexDirection:'row', paddingBottom:2, marginBottom:15}}>
             <TextInput style={styles.input} placeholder= 'Enter your name'  keyboardType="default" />
         </View>
 
@@ -55,11 +58,12 @@ const FreemiumRegister= () => {
             fontSize: 18,
             fontWeight: '500',
             color: '#333',
-            paddingLeft: 5
+            paddingLeft: 12,
+            fontWeight: "bold"
             }}>
             Email
         </Text>
-        <View style={{ paddingLeft: 20, flexDirection:'row', borderBottomColor:'#ccc', borderBottomWidth:1, paddingBottom:2, marginBottom:15}}>
+        <View style={{ paddingLeft: 10, paddingRight: 10, flexDirection:'row', paddingBottom:2, marginBottom:15}}>
             <TextInput style={styles.input} placeholder= 'Enter your email'  keyboardType="email-address" />
         </View>
 
@@ -68,24 +72,26 @@ const FreemiumRegister= () => {
             fontSize: 18,
             fontWeight: '500',
             color: '#333',
-            paddingLeft: 5
+            paddingLeft: 12,
+            fontWeight: "bold"
             }}>
             Password
         </Text>
-        <View style={{ paddingLeft: 20, flexDirection:'row', borderBottomColor:'#ccc', borderBottomWidth:1, paddingBottom:2, marginBottom:15}}>
+        <View style={{ paddingLeft: 10, paddingRight: 10, flexDirection:'row', paddingBottom:2, marginBottom:15}}>
             <TextInput style={styles.input} placeholder= 'Enter your password'  secureTextEntry={true} />
         </View>
 
         <Text style={{
             fontFamily: 'Roboto',
-            fontSize: 20,
+            fontSize: 18,
             fontWeight: '500',
             color: '#333',
-            paddingLeft: 5
+            paddingLeft: 12,
+            fontWeight: "bold"
             }}>
             Confirm Password
         </Text>
-        <View style={{ paddingLeft: 20, flexDirection:'row', borderBottomColor:'#ccc', borderBottomWidth:1, paddingBottom:2, marginBottom: 15}}>
+        <View style={{ paddingLeft: 10, paddingRight: 10, flexDirection:'row',paddingBottom:2, marginBottom: 15}}>
             <TextInput style={styles.input} placeholder= 'Enter your confirmation password' secureTextEntry={true} />
         </View>
         
@@ -94,15 +100,16 @@ const FreemiumRegister= () => {
             <Text style={styles.registerButtonText}> Register </Text>
         </TouchableOpacity>
         </View>
-        <View style={{ flexDirection:'row', borderBottomColor:'#ccc', borderBottomWidth:1, paddingBottom:2, marginBottom: 25, justifyContent: 'center'}}>
         
-        <Text style={{ alignItems:'center', textAlign: 'center', marginTop: 25}}> Already have an account? 
+        <View style={{ alignItems:'center',flexDirection:'row', marginBottom: 5, justifyContent: 'center'}}>
+        
+        <Text style={{ fontSize: 16, alignItems:'center', textAlign: 'center', marginTop: 20, fontWeight: "bold"}}> Already have an account? 
         </Text>
         </View>
         
         <View style={{ alignItems:'center'}}>
         <TouchableOpacity onPress={() => {}}>
-            <Text style={{ marginBottom: 25, justifyContent: 'center', textAlign: 'center', fontWeight: '700'}}> Log In </Text>
+            <Text style={{ fontFamily: 'Roboto', fontSize: 17, paddingBottom: 35, color: '#077167', marginTop: 15, marginBottom: 35, justifyContent: 'center', textAlign: 'center', fontWeight: 'bold'}}> Log In </Text>
         </TouchableOpacity>
         
         </View>
@@ -115,9 +122,11 @@ const FreemiumRegister= () => {
         safeArea: {flex: 1, justifyContent: 'center'},
         registerButtonContainer: {
             elevation: 6,
-            backgroundColor: "#FFFFFF",
-            borderRadius: 60,
+            backgroundColor: "#E58B68",
+            borderRadius: 12,
+            borderColor: "#000000",
             resizeBorder: 10,
+            borderWidth: 1,
             paddingVertical: 8,
             paddingHorizontal: 30
         },
@@ -126,11 +135,13 @@ const FreemiumRegister= () => {
             justifyContent: 'center',
         },
         input: {
+            fontFamily: 'Roboto',
             flex:1,
             width: 300,
             height: 40,
             backgroundColor: '#fff',
             paddingVertical: 3,
+            paddingLeft: 5,
             borderColor: '#ccc',
             borderWidth: 1,
             borderRadius: 15,
