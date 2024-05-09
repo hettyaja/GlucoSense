88671c3db93c2b2ed00db16712022f4ada3c0353
feoxy8 <nandoxu17@gmail.com>
2024-05-09 18:32:57 +0800

    Welcome page

--- src/app/index.js
+++ src/app/index.js
@@ -1,5 +1,15 @@
-import { Text } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native'
+import React from 'react'
+import { Link , Redirect} from 'expo-router'
 
-export default function index() {
-  return <Text>Home page</Text>;
+const index = () => {
+  // return (
+  //   <View className="flex-1 items-center justify-center bg-white">
+  //     <Text className="text-2xl">Hidasdsadsa</Text>
+  //     <Link href="/welcome1">Go To welcome 1</Link>
+  //   </View>
+  // );
+  return <Redirect href="welcome1"/>
 }
+
+export default index
---
+++ src/constants/images.js
@@ -0,0 +1,4 @@
+export const images = {
+    welcome1: require('../assets/welcome1.png'),
+    
+  };
