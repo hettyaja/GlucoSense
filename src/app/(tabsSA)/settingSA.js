5d5c9e923d6c34751d4921ce2aab192330a2352c
Jcj001 <joy.christensen@yahoo.com.sg>
2024-06-26 16:32:37 +0800

    sys admin in progress

--- src/app/(tabsSA)/settingSA.js
+++ src/app/(tabsSA)/settingSA.js
@@ -1,10 +1,17 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Button, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { useAuth } from '../context/authContext'
 
 const settingSA = () => {
+    const { logout } = useAuth()
+    
+    const handleSignOut = async () => {
+        await logout()
+      }
+
   return (
     <View>
-      <Text>settingSA</Text>
+      <Button title="Logout" onPress={handleSignOut} />
     </View>
   )
 }
