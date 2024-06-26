a670d6721b79c7b86da10d268a7430473aa746b5
feoxy8 <nandoxu17@gmail.com>
2024-06-27 03:04:37 +0800

    update

--- src/app/_layout.js
+++ src/app/_layout.js
@@ -17,7 +17,7 @@ const RootLayout = () => {
 
     if (isAuthenticated) {
       if (userType === 'user') {
-        router.replace('reminder')
+        router.replace('home')
       } else if (userType === 'businessPartner') {
         router.replace('homeBP')
       } else if (userType === 'systemAdmin') {
