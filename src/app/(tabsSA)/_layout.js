4328d28194e9c737b834255c6881d185de41be45
hy961 <169165271+hy961@users.noreply.github.com>
2024-06-28 16:48:33 +0800

    Update _layout.js

--- src/app/(tabsSA)/_layout.js
+++ src/app/(tabsSA)/_layout.js
@@ -1,19 +1,16 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import { Tabs } from 'expo-router'
+import React from 'react';
+import { Tabs, Stack } from 'expo-router';
 
 const _layout = () => {
   return (
     <Tabs>
       <Tabs.Screen name='insightSA' options={{headerShown:false}}/>
       <Tabs.Screen name='partnerSA' options={{headerShown:false}}/>
-      <Tabs.Screen name='settingSA' options={{headerShown:false}}/>
+      <Tabs.Screen name='settingSA' options={{headerShown:false}} />
       <Tabs.Screen name='userSA' options={{headerShown:false}}/>
-      
+      <Stack.Screen name='exportReport' options={{headerShown:false}} />
     </Tabs>
-  )
-}
+  );
+};
 
-export default _layout
-
-const styles = StyleSheet.create({})
+export default _layout;
