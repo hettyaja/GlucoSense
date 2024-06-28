31f60b7f647c1589a59b856932c5a3c7984c9106
hy961 <169165271+hy961@users.noreply.github.com>
2024-06-28 11:44:10 +0800

    Update _layout.js

--- src/app/(tabsSA)/_layout.js
+++ src/app/(tabsSA)/_layout.js
@@ -1,19 +1,29 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import { Tabs } from 'expo-router'
+import { StyleSheet } from 'react-native';
+import React from 'react';
+import { Tabs, Stack } from 'expo-router';
+import PartnerSA from './partnerSA';
+import PendingAccountList from './pendingAccountList';
+import PendingAccountDetails from './pendingAccountDetails';
 
 const _layout = () => {
   return (
     <Tabs>
-      <Tabs.Screen name='insightSA' options={{headerShown:false}}/>
-      <Tabs.Screen name='partnerSA' options={{headerShown:false}}/>
-      <Tabs.Screen name='settingSA' options={{headerShown:false}}/>
-      <Tabs.Screen name='userSA' options={{headerShown:false}}/>
-      
+      <Tabs.Screen name="insightSA" options={{ headerShown: false }} />
+      <Tabs.Screen name="partnerSA" options={{ headerShown: false }}>
+        {() => (
+          <Stack>
+            <Stack.Screen name="partnerSA" component={PartnerSA} options={{ headerShown: false }} />
+            <Stack.Screen name="pendingAccountList" component={PendingAccountList} options={{ headerShown: false }} />
+            <Stack.Screen name="pendingAccountDetails" component={PendingAccountDetails} options={{ headerShown: false }} />
+          </Stack>
+        )}
+      </Tabs.Screen>
+      <Tabs.Screen name="settingSA" options={{ headerShown: false }} />
+      <Tabs.Screen name="userSA" options={{ headerShown: false }} />
     </Tabs>
-  )
-}
+  );
+};
 
-export default _layout
+export default _layout;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
