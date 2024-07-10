86e91f19d2e32645c5ebe7c88dae049f09a7a54d
Jcj001 <joy.christensen@yahoo.com.sg>
2024-07-11 02:02:23 +0800

    in progress

--- app/Boundary/(tabsBP)/planBP.js
+++ app/Boundary/(tabsBP)/planBP.js
@@ -1,23 +1,16 @@
 import React, { useContext, useState } from 'react';
-import { View, Text, ScrollView, TouchableOpacity, Image, StyleSheet, TextInput } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
+import { View, Text, ScrollView, TouchableOpacity, TextInput, StyleSheet } from 'react-native';
 import { DietPlanContext } from '../../context/DietPlanContext';
 import DietPlanCard from '../../DietPlanCard';
 import Calendar from '../../Calendar';
 import moment from 'moment';
 import { Tabs, router } from 'expo-router';
-import MaterialIcons from 'react-native-vector-icons/MaterialIcons'
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 
 const planBP = () => {
   const { dietPlans } = useContext(DietPlanContext);
-  const navigation = useNavigation();
-
   const [startDate, setStartDate] = useState(null);
   const [endDate, setEndDate] = useState(null);
-
-  const handleCreateDietPlan = () => {
-    navigation.navigate('CreateDietPlan');
-  };
 
   const handleConfirm = ({ startDate, endDate }) => {
     setStartDate(startDate);
@@ -33,37 +26,34 @@ const planBP = () => {
 
   return (
     <>
-    <Tabs.Screen options={{
+      <Tabs.Screen options={{
         title: 'Diet Plan',
         headerStyle: { backgroundColor: '#E58B68' },
         headerTitleStyle: { color: 'white', fontFamily: 'Poppins-Bold'},
         headerTitle: 'Diet Plan',
         headerTitleAlign: 'center',
         headerRight: () => (
-          <TouchableOpacity onPress={() => router.push('CreateDietPlan')} style={{marginRight:16}}>
+          <TouchableOpacity onPress={() => router.push('/CreateDietPlan')} style={{ marginRight: 16 }}>
             <MaterialIcons name='add' size={32} color='white'/>
           </TouchableOpacity>
         )
       }}/>
 
-    <ScrollView contentContainerStyle={styles.container}>
-      <View style={styles.searchContainer}>
-        <TextInput style={styles.searchInput} placeholder="Search & Filter" />
-      </View>
-      <Calendar onConfirm={handleConfirm} />
-      {dietPlans.map((dietPlan) => (
-        <DietPlanCard key={dietPlan.id} dietPlan={dietPlan} />
-      ))}
-    </ScrollView>
+      <ScrollView contentContainerStyle={styles.container}>
+        <View style={styles.searchContainer}>
+          <TextInput style={styles.searchInput} placeholder="Search & Filter" />
+        </View>
+        <Calendar onConfirm={handleConfirm} />
+        {dietPlans.map((dietPlan) => (
+          <DietPlanCard key={dietPlan.id} dietPlan={dietPlan} />
+        ))}
+      </ScrollView>
     </>
   );
 };
 
 const styles = StyleSheet.create({
   container: { padding: 20 },
-  header: { flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', backgroundColor: '#F28B54', paddingVertical: 10, paddingHorizontal: 15 },
-  icon: { width: 24, height: 24 },
-  title: { fontSize: 17, fontWeight: 'bold', color: '#FFFFFF' },
   searchContainer: { marginTop: 20 },
   searchInput: { borderWidth: 1, borderColor: '#ccc', borderRadius: 15, paddingHorizontal: 10, height: 40 },
 });
--- app/CreateDietPlan.js
+++ app/CreateDietPlan.js
@@ -1,15 +1,13 @@
 import React, { useState, useContext } from 'react';
-import {SafeAreaView, StyleSheet, Text, TextInput, TouchableOpacity,View, Image, ScrollView} from 'react-native';
+import { SafeAreaView, StyleSheet, Text, TextInput, TouchableOpacity, View, Image, ScrollView } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
-import { useNavigation } from '@react-navigation/native';
 import { DietPlanContext } from './context/DietPlanContext';
 import { Picker } from '@react-native-picker/picker';
-import { Stack, router } from 'expo-router';
-import Ionicons from 'react-native-vector-icons/Ionicons'
+import { Stack, useRouter } from 'expo-router';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 const CreateDietPlan = () => {
-  const { addDietPlan} = useContext(DietPlanContext);
-  const navigation = useNavigation();
+  const { addDietPlan } = useContext(DietPlanContext);
   const [day, setDay] = useState('');
   const [lunchImage, setLunchImage] = useState(null);
   const [dinnerImage, setDinnerImage] = useState(null);
@@ -19,6 +17,7 @@ const CreateDietPlan = () => {
   const [dinnerDescription, setDinnerDescription] = useState('');
   const [lunchIngredients, setLunchIngredients] = useState('');
   const [dinnerIngredients, setDinnerIngredients] = useState('');
+  const router = useRouter();
 
   const addImage = async (setImage) => {
     const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
@@ -59,80 +58,80 @@ const CreateDietPlan = () => {
       },
     };
     addDietPlan(newDietPlan);
-    navigation.navigate('ViewAndSearchDietPlan');
+    router.push('/planBP'); // Ensure this path is correct
   };
 
   return (
     <>
       <Stack.Screen options={{
-      title: 'Create Menu & Recipe',
-      headerStyle: { backgroundColor: '#E58B68' },
-      headerTitleStyle: { color: 'white', fontFamily: 'Poppins-Bold'},
-      headerTitle: 'Create Menu & Recipe',
-      headerTitleAlign: 'center',
-      headerLeft: () => (
-        <TouchableOpacity onPress={() => router.back('foodBP')}>
-          <Ionicons name='chevron-back' size={32} color='white'/>
-        </TouchableOpacity>
-      ),
-      headerRight: () => (
-        <TouchableOpacity onPress={() => handleSave()}>
-          <Text style={{fontFamily: 'Poppins-SemiBold', fontSize:14, color:'white'}}>Publish</Text>
-        </TouchableOpacity>
-      ),
-    }}/>
-
-    <SafeAreaView style={styles.safeArea}>
-      <ScrollView contentContainerStyle={styles.container}>
-        <View style={styles.inputContainer}>
-          <Text style={styles.label}>Day</Text>
-          <Picker selectedValue={day} onValueChange={(itemValue) => setDay(itemValue)} style={styles.picker}>
-            <Picker.Item label="Select day" value="" />
-            <Picker.Item label="Monday" value="Monday" />
-            <Picker.Item label="Tuesday" value="Tuesday" />
-            <Picker.Item label="Wednesday" value="Wednesday" />
-            <Picker.Item label="Thursday" value="Thursday" />
-            <Picker.Item label="Friday" value="Friday" />
-            <Picker.Item label="Saturday" value="Saturday" />
-            <Picker.Item label="Sunday" value="Sunday" />
-          </Picker>
-        </View>
-
-        <View style={styles.mealContainer}>
-          <Text style={styles.mealTitle}>Lunch</Text>
-          <TouchableOpacity onPress={() => addImage(setLunchImage)} style={styles.imageUpload}>
-            {lunchImage ? (
-              <Image source={{ uri: lunchImage }} style={styles.uploadedImage} />
-            ) : (
-              <View style={styles.uploadPlaceholder}>
-                <Image source={require('./assets/iconCarrier.png')} style={styles.uploadIcon} />
-                <Text>Upload recipe photo</Text>
-              </View>
-            )}
+        title: 'Create Menu & Recipe',
+        headerStyle: { backgroundColor: '#E58B68' },
+        headerTitleStyle: { color: 'white', fontFamily: 'Poppins-Bold'},
+        headerTitle: 'Create Menu & Recipe',
+        headerTitleAlign: 'center',
+        headerLeft: () => (
+          <TouchableOpacity onPress={() => router.back()}>
+            <Ionicons name='chevron-back' size={32} color='white'/>
           </TouchableOpacity>
-          <TextInput placeholder="Name" value={lunchTitle} onChangeText={setLunchTitle} style={styles.input} />
-          <TextInput placeholder="Description" value={lunchDescription} onChangeText={setLunchDescription} style={styles.input} />
-          <TextInput placeholder="Ingredients" value={lunchIngredients} onChangeText={setLunchIngredients} style={styles.input} />
-        </View>
-
-        <View style={styles.mealContainer}>
-          <Text style={styles.mealTitle}>Dinner</Text>
-          <TouchableOpacity onPress={() => addImage(setDinnerImage)} style={styles.imageUpload}>
-            {dinnerImage ? (
-              <Image source={{ uri: dinnerImage }} style={styles.uploadedImage} />
-            ) : (
-              <View style={styles.uploadPlaceholder}>
-                <Image source={require('./assets/iconCarrier.png')} style={styles.uploadIcon} />
-                <Text>Upload recipe photo</Text>
-              </View>
-            )}
+        ),
+        headerRight: () => (
+          <TouchableOpacity onPress={handleSave}>
+            <Text style={{ fontFamily: 'Poppins-SemiBold', fontSize: 14, color: 'white' }}>Publish</Text>
           </TouchableOpacity>
-          <TextInput placeholder="Name" value={dinnerTitle} onChangeText={setDinnerTitle} style={styles.input} />
-          <TextInput placeholder="Description" value={dinnerDescription} onChangeText={setDinnerDescription} style={styles.input} />
-          <TextInput placeholder="Ingredients" value={dinnerIngredients} onChangeText={setDinnerIngredients} style={styles.input} />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+        ),
+      }}/>
+
+      <SafeAreaView style={styles.safeArea}>
+        <ScrollView contentContainerStyle={styles.container}>
+          <View style={styles.inputContainer}>
+            <Text style={styles.label}>Day</Text>
+            <Picker selectedValue={day} onValueChange={(itemValue) => setDay(itemValue)} style={styles.picker}>
+              <Picker.Item label="Select day" value="" />
+              <Picker.Item label="Monday" value="Monday" />
+              <Picker.Item label="Tuesday" value="Tuesday" />
+              <Picker.Item label="Wednesday" value="Wednesday" />
+              <Picker.Item label="Thursday" value="Thursday" />
+              <Picker.Item label="Friday" value="Friday" />
+              <Picker.Item label="Saturday" value="Saturday" />
+              <Picker.Item label="Sunday" value="Sunday" />
+            </Picker>
+          </View>
+
+          <View style={styles.mealContainer}>
+            <Text style={styles.mealTitle}>Lunch</Text>
+            <TouchableOpacity onPress={() => addImage(setLunchImage)} style={styles.imageUpload}>
+              {lunchImage ? (
+                <Image source={{ uri: lunchImage }} style={styles.uploadedImage} />
+              ) : (
+                <View style={styles.uploadPlaceholder}>
+                  <Image source={require('./assets/iconCarrier.png')} style={styles.uploadIcon} />
+                  <Text>Upload recipe photo</Text>
+                </View>
+              )}
+            </TouchableOpacity>
+            <TextInput placeholder="Name" value={lunchTitle} onChangeText={setLunchTitle} style={styles.input} />
+            <TextInput placeholder="Description" value={lunchDescription} onChangeText={setLunchDescription} style={styles.input} />
+            <TextInput placeholder="Ingredients" value={lunchIngredients} onChangeText={setLunchIngredients} style={styles.input} />
+          </View>
+
+          <View style={styles.mealContainer}>
+            <Text style={styles.mealTitle}>Dinner</Text>
+            <TouchableOpacity onPress={() => addImage(setDinnerImage)} style={styles.imageUpload}>
+              {dinnerImage ? (
+                <Image source={{ uri: dinnerImage }} style={styles.uploadedImage} />
+              ) : (
+                <View style={styles.uploadPlaceholder}>
+                  <Image source={require('./assets/iconCarrier.png')} style={styles.uploadIcon} />
+                  <Text>Upload recipe photo</Text>
+                </View>
+              )}
+            </TouchableOpacity>
+            <TextInput placeholder="Name" value={dinnerTitle} onChangeText={setDinnerTitle} style={styles.input} />
+            <TextInput placeholder="Description" value={dinnerDescription} onChangeText={setDinnerDescription} style={styles.input} />
+            <TextInput placeholder="Ingredients" value={dinnerIngredients} onChangeText={setDinnerIngredients} style={styles.input} />
+          </View>
+        </ScrollView>
+      </SafeAreaView>
     </>
   );
 };
@@ -140,10 +139,6 @@ const CreateDietPlan = () => {
 const styles = StyleSheet.create({
   safeArea: { flex: 1 },
   container: { padding: 20 },
-  header: { flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', backgroundColor: '#F28B54', paddingVertical: 10, paddingHorizontal: 15 },
-  icon: { width: 24, height: 24 },
-  title: { fontSize: 17, fontWeight: 'bold', color: '#FFFFFF' },
-  saveText: { fontSize: 17, fontWeight: 'bold', color: '#FFFFFF' },
   inputContainer: { marginVertical: 10 },
   picker: { height: 50, width: '100%' },
   mealContainer: { marginTop: 20 },
