74f9b73356fee9445377245e24839b1c9dfea9f8
hy961 <169165271+hy961@users.noreply.github.com>
2024-07-15 21:10:13 +0800

    Update _layout.js

--- app/Boundary/(tabsSA)/_layout.js
+++ app/Boundary/(tabsSA)/_layout.js
@@ -1,69 +1,63 @@
 import React from 'react';
+import { Text } from 'react-native'
 import { Tabs, Stack } from 'expo-router';
-import SimpleLineIcons from 'react-native-vector-icons/SimpleLineIcons';
-import Ionicons from 'react-native-vector-icons/Ionicons';
-import Feather from 'react-native-vector-icons/Feather';
-import AntDesign from 'react-native-vector-icons/AntDesign';
+import SimpleLineIcons from 'react-native-vector-icons/SimpleLineIcons'
+import Ionicons from 'react-native-vector-icons/Ionicons'
+import Feather from 'react-native-vector-icons/Feather'
+import AntDesign from 'react-native-vector-icons/AntDesign'
 
 const _layout = () => {
   return (
     <Tabs>
-      <Tabs.Screen name="insightSA" options={{
-        headerShown: false,
-        title: 'Insight',
+      <Tabs.Screen name='insightSA' options={{
+        headerShown:false,
+        title:'Insight',
         tabBarIcon: () => (
-          <SimpleLineIcons name="graph" size={24} color="#E58B68" />
+          <SimpleLineIcons name='graph' size={24} color='#E58B68'/>
         ),
         tabBarLabel: ({ focused }) => (
           <Text style={{ fontFamily: focused ? 'Poppins-SemiBold' : 'Poppins-Regular', color: '#E58B68' }}>
-            Insight
+              Insight
           </Text>
         ),
-      }} />
-      <Tabs.Screen name="userSA" options={{
-        headerShown: false,
-        title: 'Users',
+      }}/>
+      
+      <Tabs.Screen name='userSA' options={{
+        headerShown:false,
+        title:'Users',
         tabBarIcon: () => (
-          <Feather name="user" size={24} color="#E58B68" />
+          <Feather name='user' size={24} color='#E58B68'/>
         ),
         tabBarLabel: ({ focused }) => (
           <Text style={{ fontFamily: focused ? 'Poppins-SemiBold' : 'Poppins-Regular', color: '#E58B68' }}>
-            Users
+              Users
           </Text>
         ),
-      }} />
-      <Tabs.Screen name="partnerSA" options={{
-        headerShown: false,
-        title: 'Partners',
+      }}/>
+      <Tabs.Screen name='partnerSA' options={{
+        headerShown:false,
+        title:'Partners',
         tabBarIcon: () => (
-          <Ionicons name="storefront-outline" size={24} color="#E58B68" />
+          <Ionicons name='storefront-outline' size={24} color='#E58B68'/>
         ),
         tabBarLabel: ({ focused }) => (
           <Text style={{ fontFamily: focused ? 'Poppins-SemiBold' : 'Poppins-Regular', color: '#E58B68' }}>
-            Partners
+              Partners
           </Text>
         ),
-      }}>
-        {() => (
-          <Stack>
-            <Stack.Screen name="index" component={PartnerSA} options={{ headerShown: false }} />
-            <Stack.Screen name="pendingAccountList" component={PendingAccountList} options={{ headerShown: false }} />
-            <Stack.Screen name="pendingAccountDetails" component={PendingAccountDetails} options={{ headerShown: false }} />
-          </Stack>
-        )}
-      </Tabs.Screen>
-      <Tabs.Screen name="settingSA" options={{
-        headerShown: false,
-        title: 'Setting',
+        }}/>
+      <Tabs.Screen name='settingSA' options={{
+        headerShown:false,
+        title:'Setting',
         tabBarIcon: () => (
-          <AntDesign name="setting" size={24} color="#E58B68" />
+          <AntDesign name='setting' size={24} color='#E58B68'/>
         ),
         tabBarLabel: ({ focused }) => (
           <Text style={{ fontFamily: focused ? 'Poppins-SemiBold' : 'Poppins-Regular', color: '#E58B68' }}>
-            Setting
+              Setting
           </Text>
         ),
-      }} />
+        }} />
     </Tabs>
   );
 };
