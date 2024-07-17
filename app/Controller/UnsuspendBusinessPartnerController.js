7b5a09a54a3232f9d533e045082de9c35fba467c
hy961 <169165271+hy961@users.noreply.github.com>
2024-07-17 21:17:46 +0800

    Update UnsuspendBusinessPartnerController.js

--- app/Controller/UnsuspendBusinessPartnerController.js
+++ app/Controller/UnsuspendBusinessPartnerController.js
@@ -1,11 +1,11 @@
 import BusinessPartner from "../Entity/BusinessPartner";
 
 class UnsuspendBusinessPartnerController {
-  static async unsuspend(uid) {
+  static async unsuspend(id) {
     try {
-      return await BusinessPartner.unsuspendBusinessPartner(uid);
+      return await BusinessPartner.unsuspendBusinessPartner(id);
     } catch (error) {
-      console.error('Error unsuspending business partner:', error);
+      console.error("Error unsuspending business partner: ", error);
       throw new Error('Failed to unsuspend business partner.');
     }
   }
