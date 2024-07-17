886876a4b97b47737d64e251cd4de6749e2ca498
hy961 <169165271+hy961@users.noreply.github.com>
2024-07-17 10:19:59 +0800

    Create SuspendBusinessPartnerController.js

---
+++ app/Controller/SuspendBusinessPartnerController.js
@@ -0,0 +1,14 @@
+import BusinessPartner from "../Entity/BusinessPartner";
+
+class SuspendBusinessPartnerController {
+  static async suspend(uid) {
+    try {
+      return await BusinessPartner.suspendBusinessPartner(uid);
+    } catch (error) {
+      console.error("Error suspending business partner: ", error);
+      throw new Error('Failed to suspend business partner.');
+    }
+  }
+}
+
+export default SuspendBusinessPartnerController;
