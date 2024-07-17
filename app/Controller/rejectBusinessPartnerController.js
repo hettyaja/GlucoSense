2810d5b152a8133c4df209bed8499c7356362022
hy961 <169165271+hy961@users.noreply.github.com>
2024-07-17 10:17:11 +0800

    Update rejectBusinessPartnerController.js

--- app/Controller/rejectBusinessPartnerController.js
+++ app/Controller/rejectBusinessPartnerController.js
@@ -1,1 +1,15 @@
+import BusinessPartner from "../Entity/BusinessPartner";
+
+class RejectBusinessPartnerController {
+  static async reject(uid) {
+    try {
+      return await BusinessPartner.rejectBusinessPartner(uid);
+    } catch (error) {
+      console.error("Error rejecting business partner: ", error);
+      throw new Error('Failed to reject business partner.');
+    }
+  }
+}
+
+export default RejectBusinessPartnerController;
 
