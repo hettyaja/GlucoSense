60182535603715529312adb22e7a46950f8a7354
hy961 <169165271+hy961@users.noreply.github.com>
2024-07-17 21:30:29 +0800

    Update ApproveBusinessPartnerController.js

--- app/Controller/ApproveBusinessPartnerController.js
+++ app/Controller/ApproveBusinessPartnerController.js
@@ -1,9 +1,9 @@
-import BusinessPartner from "../Entity/BusinessPartner";
+import BusinessPartner from '../Entity/BusinessPartner';
 
 class ApproveBusinessPartnerController {
-  static async approve(uid) {
+  static async approve(accountId) {
     try {
-      return await BusinessPartner.approveBusinessPartner(uid);
+      return await BusinessPartner.approveBusinessPartner(accountId);
     } catch (error) {
       console.error('Error approving business partner:', error);
       throw new Error('Failed to approve business partner.');
