b8a054267ec3601ae2542c0cbf89a41503e4c937
hy961 <169165271+hy961@users.noreply.github.com>
2024-07-17 21:34:57 +0800

    Update ViewPendingAccountDetailsController.js

--- app/Controller/ViewPendingAccountDetailsController.js
+++ app/Controller/ViewPendingAccountDetailsController.js
@@ -1,12 +1,12 @@
-import BusinessPartner from "../Entity/BusinessPartner";
+import BusinessPartner from '../Entity/BusinessPartner';
 
 class ViewPendingAccountDetailsController {
-  static async getPendingDetails(uid) {
+  static async getDetails(accountId) {
     try {
-      return await BusinessPartner.getPendingDetails(uid);
+      return await BusinessPartner.getPendingDetails(accountId);
     } catch (error) {
-      console.error('Error fetching pending account details:', error);
-      throw new Error('Failed to fetch pending account details.');
+      console.error('Error fetching account details:', error);
+      throw new Error('Failed to fetch account details.');
     }
   }
 }
