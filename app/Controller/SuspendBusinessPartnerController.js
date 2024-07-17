6c302072eeabc40d37e4695088c4a96c3e6b77e4
hy961 <169165271+hy961@users.noreply.github.com>
2024-07-17 10:51:38 +0800

    Update SuspendBusinessPartnerController.js

--- app/Controller/SuspendBusinessPartnerController.js
+++ app/Controller/SuspendBusinessPartnerController.js
@@ -5,7 +5,7 @@ class SuspendBusinessPartnerController {
     try {
       return await BusinessPartner.suspendBusinessPartner(uid);
     } catch (error) {
-      console.error("Error suspending business partner: ", error);
+      console.error('Error suspending business partner:', error);
       throw new Error('Failed to suspend business partner.');
     }
   }
