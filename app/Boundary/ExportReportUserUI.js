d6856c71b32a9672356330ef1d4c34880c103568
feoxy8 <nandoxu17@gmail.com>
2024-07-24 00:25:12 +0800

    update

--- app/Boundary/ExportReportUserUI.js
+++ app/Boundary/ExportReportUserUI.js
@@ -1,9 +1,9 @@
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View, Alert, Platform } from 'react-native';
 import React, { useEffect, useState } from 'react';
 import { printToFileAsync } from 'expo-print';
-import { shareAsync } from 'expo-sharing';
-import * as Sharing from 'expo-sharing';
 import * as FileSystem from 'expo-file-system';
+import * as MediaLibrary from 'expo-media-library';
+import { StorageAccessFramework } from 'expo-file-system';
 import Header from '../components/Header';
 import { router } from 'expo-router';
 import ViewGlucoseLogsController from '../Controller/ViewGlucoseLogsController';
@@ -12,6 +12,7 @@ import ViewMedicineLogsController from '../Controller/ViewMedicineLogsController
 import { useAuth } from '../service/AuthContext';
 import { Picker } from '@react-native-picker/picker';
 import XLSX from 'xlsx';
+import DateTimePicker from '@react-native-community/datetimepicker';
 
 const exportReportUserUI = () => {
   const { user } = useAuth();
@@ -19,6 +20,11 @@ const exportReportUserUI = () => {
   const [medicineLogs, setMedicineLogs] = useState([]);
   const [mealLogs, setMealLogs] = useState([]);
   const [selectedLogType, setSelectedLogType] = useState('Glucose');
+  const [selectedFormat, setSelectedFormat] = useState('pdf');
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
+  const [showStartDatePicker, setShowStartDatePicker] = useState(false);
+  const [showEndDatePicker, setShowEndDatePicker] = useState(false);
 
   useEffect(() => {
     const fetchLogs = async () => {
@@ -39,29 +45,104 @@ const exportReportUserUI = () => {
     fetchLogs();
   }, [user]);
 
+  const filterLogsByDateRange = (logs) => {
+    const start = new Date(startDate).setHours(0, 0, 0, 0);
+    const end = new Date(endDate).setHours(23, 59, 59, 999);
+
+    return logs.filter(log => {
+      const logDate = new Date(log.time.seconds * 1000).setHours(0, 0, 0, 0);
+      return logDate >= start && logDate <= end;
+    });
+  };
+
   const convertTimestamp = (timestamp) => {
     const date = new Date(timestamp.seconds * 1000 + timestamp.nanoseconds / 1000000);
     const dateString = date.toLocaleDateString();
     const timeString = date.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
-    const period = date.getHours() >= 12 ? 'PM' : 'AM';
-    return { dateString, timeString, period };
+    return { dateString, timeString };
   };
 
-  const generateTableRows = (logs) => {
+  const generateTableHeaders = (type) => {
+    if (type === 'Glucose') {
+      return `
+        <tr>
+          <th>Date</th>
+          <th>Time</th>
+          <th>Period</th>
+          <th>Blood Sugar</th>
+          <th>Notes</th>
+        </tr>
+      `;
+    } else if (type === 'Meal') {
+      return `
+        <tr>
+          <th>Date</th>
+          <th>Time</th>
+          <th>Period</th>
+          <th>Calories</th>
+          <th>Carbs</th>
+          <th>Protein</th>
+          <th>Fat</th>
+          <th>Notes</th>
+        </tr>
+      `;
+    } else if (type === 'Medicine') {
+      return `
+        <tr>
+          <th>Date</th>
+          <th>Time</th>
+          <th>Medicine</th>
+          <th>Notes</th>
+        </tr>
+      `;
+    }
+  };
+
+  const generateTableRows = (logs, type) => {
     return logs.map(log => {
-      const { dateString, timeString, period } = convertTimestamp(log.time);
-      return {
-        Date: dateString,
-        Time: timeString,
-        Period: period,
-        [selectedLogType === 'Glucose' ? 'Blood Sugar' : selectedLogType === 'Meal' ? 'Meal' : 'Medicine']: log[selectedLogType.toLowerCase()],
-        Notes: log.notes || '',
-      };
-    });
+      const { dateString, timeString } = convertTimestamp(log.time);
+      if (type === 'Glucose') {
+        return `
+          <tr>
+            <td>${dateString}</td>
+            <td>${timeString}</td>
+            <td>${log.period}</td>
+            <td>${log.glucose}</td>
+            <td>${log.notes || ''}</td>
+          </tr>
+        `;
+      } else if (type === 'Meal') {
+        return `
+          <tr>
+            <td>${dateString}</td>
+            <td>${timeString}</td>
+            <td>${log.period}</td>
+            <td>${log.calories}</td>
+            <td>${log.carbs}</td>
+            <td>${log.protein}</td>
+            <td>${log.fat}</td>
+            <td>${log.notes || ''}</td>
+          </tr>
+        `;
+      } else if (type === 'Medicine') {
+        const medicineString = Object.entries(log.medicine)
+          .map(([name, amount]) => `${name}: ${amount}mg`)
+          .join(', ');
+        return `
+          <tr>
+            <td>${dateString}</td>
+            <td>${timeString}</td>
+            <td>${medicineString}</td>
+            <td>${log.notes || ''}</td>
+          </tr>
+        `;
+      }
+    }).join('');
   };
 
   const generateHtml = (type, logs) => {
-    const tableRows = generateTableRows(logs);
+    const tableHeaders = generateTableHeaders(type);
+    const tableRows = generateTableRows(logs, type);
 
     return `
       <html>
@@ -97,22 +178,8 @@ const exportReportUserUI = () => {
       <body>
           <h1>${type.toUpperCase()} LOGS</h1>
           <table>
-              <tr>
-                  <th>Date</th>
-                  <th>Time</th>
-                  <th>Period</th>
-                  <th>${type === 'Glucose' ? 'Blood Sugar' : type === 'Meal' ? 'Meal' : 'Medicine'}</th>
-                  <th>Notes</th>
-              </tr>
-              ${tableRows.map(row => `
-                <tr>
-                  <td>${row.Date}</td>
-                  <td>${row.Time}</td>
-                  <td>${row.Period}</td>
-                  <td>${row[selectedLogType === 'Glucose' ? 'Blood Sugar' : selectedLogType === 'Meal' ? 'Meal' : 'Medicine']}</td>
-                  <td>${row.Notes}</td>
-                </tr>
-              `).join('')}
+              ${tableHeaders}
+              ${tableRows}
           </table>
           <div class="footer">
               For more printable ${type.toLowerCase()} log sheets, please visit https://inkpx.com
@@ -122,47 +189,79 @@ const exportReportUserUI = () => {
     `;
   };
 
-  const handlePrint = async () => {
+  const pickDirectoryAndSaveFile = async (fileName, fileData, mimeType) => {
     try {
-      let logs;
-      if (selectedLogType === 'Glucose') logs = glucoseLogs;
-      else if (selectedLogType === 'Meal') logs = mealLogs;
-      else if (selectedLogType === 'Medicine') logs = medicineLogs;
+      const permissions = await StorageAccessFramework.requestDirectoryPermissionsAsync();
+      if (!permissions.granted) {
+        Alert.alert('Error', 'Directory permission not granted');
+        return;
+      }
 
-      const html = generateHtml(selectedLogType, logs);
-      const { uri } = await printToFileAsync({ html });
-      await shareAsync(uri, { mimeType: 'application/pdf' });
-    } catch (error) {
-      console.error('Error generating or sharing PDF:', error);
+      const uri = await StorageAccessFramework.createFileAsync(permissions.directoryUri, fileName, mimeType);
+      await FileSystem.writeAsStringAsync(uri, fileData, { encoding: FileSystem.EncodingType.Base64 });
+      Alert.alert('Success', `File saved to ${uri}`);
+    } catch (e) {
+      console.error('Error saving file:', e);
+      Alert.alert('Error', 'An error occurred while saving the file.');
     }
   };
 
-  const exportToExcel = async () => {
+  const exportAsPdf = async () => {
     try {
       let logs;
-      if (selectedLogType === 'Glucose') logs = glucoseLogs;
-      else if (selectedLogType === 'Meal') logs = mealLogs;
-      else if (selectedLogType === 'Medicine') logs = medicineLogs;
+      if (selectedLogType === 'Glucose') logs = filterLogsByDateRange(glucoseLogs);
+      else if (selectedLogType === 'Meal') logs = filterLogsByDateRange(mealLogs);
+      else if (selectedLogType === 'Medicine') logs = filterLogsByDateRange(medicineLogs);
 
-      const tableRows = generateTableRows(logs);
-      const ws = XLSX.utils.json_to_sheet(tableRows);
+      const html = generateHtml(selectedLogType, logs);
+      const { uri } = await printToFileAsync({ html });
+      const base64Data = await FileSystem.readAsStringAsync(uri, { encoding: FileSystem.EncodingType.Base64 });
+      await pickDirectoryAndSaveFile(`${selectedLogType}_Logs.pdf`, base64Data, 'application/pdf');
+    } catch (error) {
+      console.error('Error generating or saving PDF:', error);
+      Alert.alert('Error', 'An error occurred while generating or saving the PDF file.');
+    }
+  };
+
+  const exportAsExcel = async () => {
+    try {
+      let logs;
+      if (selectedLogType === 'Glucose') logs = filterLogsByDateRange(glucoseLogs);
+      else if (selectedLogType === 'Meal') logs = filterLogsByDateRange(mealLogs);
+      else if (selectedLogType === 'Medicine') logs = filterLogsByDateRange(medicineLogs);
+
+      const tableRows = generateTableRows(logs, selectedLogType);
+      const ws = XLSX.utils.json_to_sheet(tableRows.map(row => JSON.parse(row.match(/{.*?}/)[0])));
       const wb = XLSX.utils.book_new();
       XLSX.utils.book_append_sheet(wb, ws, 'Logs');
 
       const wbout = XLSX.write(wb, { type: 'base64', bookType: 'xlsx' });
-
-      const uri = FileSystem.cacheDirectory + `${selectedLogType}_Logs.xlsx`;
-      await FileSystem.writeAsStringAsync(uri, wbout, {
-        encoding: FileSystem.EncodingType.Base64,
-      });
-
-      await Sharing.shareAsync(uri, {
-        mimeType: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
-        dialogTitle: 'Export Excel',
-        UTI: 'com.microsoft.excel.xlsx',
-      });
+      await pickDirectoryAndSaveFile(`${selectedLogType}_Logs.xlsx`, wbout, 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
     } catch (error) {
       console.error('Error exporting to Excel:', error);
+      Alert.alert('Error', 'An error occurred while exporting the Excel file.');
+    }
+  };
+
+  const handleExport = async () => {
+    if (selectedFormat === 'pdf') {
+      await exportAsPdf();
+    } else if (selectedFormat === 'xlsx') {
+      await exportAsExcel();
+    }
+  };
+
+  const onStartDateChange = (event, selectedDate) => {
+    setShowStartDatePicker(false);
+    if (selectedDate !== undefined) {
+      setStartDate(selectedDate);
+    }
+  };
+
+  const onEndDateChange = (event, selectedDate) => {
+    setShowEndDatePicker(false);
+    if (selectedDate !== undefined) {
+      setEndDate(selectedDate);
     }
   };
 
@@ -174,20 +273,67 @@ const exportReportUserUI = () => {
         onLeftButtonPress={() => router.back()}
       />
       <View style={styles.container}>
-        <Picker
-          selectedValue={selectedLogType}
-          style={styles.picker}
-          onValueChange={(itemValue) => setSelectedLogType(itemValue)}
-        >
-          <Picker.Item label="Glucose" value="Glucose" />
-          <Picker.Item label="Meal" value="Meal" />
-          <Picker.Item label="Medicine" value="Medicine" />
-        </Picker>
-        <TouchableOpacity onPress={handlePrint} style={styles.button}>
-          <Text style={styles.buttonText}>Export PDF</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={exportToExcel} style={styles.button}>
-          <Text style={styles.buttonText}>Export Excel</Text>
+        <Text style={styles.sectionText}>Report</Text>
+        <View style={styles.section}>
+          <View style={styles.item}>
+            <Text style={styles.itemText}>Data</Text>
+            <Picker
+              selectedValue={selectedLogType}
+              style={styles.picker}
+              onValueChange={(itemValue) => setSelectedLogType(itemValue)}
+            >
+              <Picker.Item label="Glucose" value="Glucose" />
+              <Picker.Item label="Meal" value="Meal" />
+              <Picker.Item label="Medicine" value="Medicine" />
+            </Picker>
+          </View>
+          <View style={styles.divider} />
+          <View style={styles.item}>
+            <Text style={styles.itemText}>Format</Text>
+            <Picker
+              selectedValue={selectedFormat}
+              style={styles.picker}
+              onValueChange={(itemValue) => setSelectedFormat(itemValue)}
+            >
+              <Picker.Item label="PDF" value="pdf"/>
+              <Picker.Item label="Excel" value="xlsx"/>
+            </Picker>
+          </View>
+        </View>
+        <Text style={styles.sectionText}>Period</Text>
+        <View style={styles.section}>
+          <View style={styles.item}>
+            <Text style={styles.itemText}>Start Date</Text>
+            <TouchableOpacity onPress={() => setShowStartDatePicker(true)}>
+              <Text style={styles.itemText}>{startDate.toLocaleDateString()}</Text>
+            </TouchableOpacity>
+            {showStartDatePicker && (
+              <DateTimePicker
+                value={startDate}
+                mode="date"
+                display="default"
+                onChange={onStartDateChange}
+              />
+            )}
+          </View>
+          <View style={styles.divider} />
+          <View style={styles.item}>
+            <Text style={styles.itemText}>End Date</Text>
+            <TouchableOpacity onPress={() => setShowEndDatePicker(true)}>
+              <Text style={styles.itemText}>{endDate.toLocaleDateString()}</Text>
+            </TouchableOpacity>
+            {showEndDatePicker && (
+              <DateTimePicker
+                value={endDate}
+                mode="date"
+                display="default"
+                onChange={onEndDateChange}
+              />
+            )}
+          </View>
+        </View>
+        <TouchableOpacity onPress={handleExport} style={styles.button}>
+          <Text style={styles.buttonText}>Export</Text>
         </TouchableOpacity>
       </View>
     </>
@@ -199,22 +345,53 @@ export default exportReportUserUI;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    backgroundColor: '#f5f5f5'
+  },
+  section: {
+    backgroundColor: 'white',
+    borderTopWidth: 0.5,
+    borderBottomWidth: 0.5,
+  },
+  sectionText: {
+    fontFamily: 'Poppins-Regular',
+    fontSize: 12,
+    color: '#808080',
+    paddingHorizontal: 16,
+    paddingTop: 16,
+  },
+  item: {
+    flexDirection: 'row',
     alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  itemText: {
+    fontFamily: 'Poppins-Regular',
+    fontSize: 12,
+    color: 'black',
+    paddingHorizontal: 16,
+    paddingVertical: 16,
   },
   picker: {
-    height: 50,
+    height: 40,
     width: 150,
-    marginVertical: 10,
   },
   button: {
-    backgroundColor: '#E58B68',
-    padding: 15,
-    borderRadius: 5,
-    marginVertical: 5,
+    backgroundColor: 'white',
+    marginVertical: 16,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderTopWidth: 0.5,
+    borderBottomWidth: 0.5,
   },
   buttonText: {
-    color: '#FFF',
-    fontSize: 16,
+    fontFamily: 'Poppins-SemiBold',
+    fontSize: 12,
+    color: '#E58B68',
+    padding: 16,
+  },
+  divider: {
+    height: 0.5,
+    backgroundColor: 'black',
+    marginHorizontal: 16,
   },
 });
